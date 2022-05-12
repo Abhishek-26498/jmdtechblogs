2723ab82ed94702865233f825233c0fa07b48194
Abhishek-260498 <dhiman26abhishek@gmail.com>
2022-05-12 16:58:46 -0700

    new

--- pages/blog.js
+++ pages/blog.js
@@ -36,7 +36,8 @@ const Blog = (props) => {
   )
 }
 export async function getServerSideProps(context) {
-  const res = await fetch('/api/blogs')
+  const { URL } = process.env;
+  const res = await fetch(URL+'/api/blogs')
   const data = await res.json()
   return {
     props: { data }, // will be passed to the page component as props
