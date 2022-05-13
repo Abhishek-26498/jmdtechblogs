a54584fca62ed4bd6cd1cd658a7900ae3a506fdd
Abhishek-260498 <dhiman26abhishek@gmail.com>
2022-05-12 17:30:48 -0700

    new

--- component/Add.js
+++ component/Add.js
@@ -25,8 +25,8 @@ const Add = ({ setClose }) => {
     console.log(title, content, author, slug)
 
     const data = {title, content, author, slug}
-
-   await fetch('http://localhost:3000/api/blogs ', {
+    const { URL } = process.env; 
+   await fetch(URL+'/api/blogs ', {
       method: 'POST', // or 'PUT'
       headers: {
         'Content-Type': 'application/json',
