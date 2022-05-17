389f37223b39a84bf72b5b542ee5ac1a5b925f9b
Abhishek-260498 <dhiman26abhishek@gmail.com>
2022-05-17 16:58:47 -0700

    new

--- pages/blog.js
+++ pages/blog.js
@@ -23,12 +23,13 @@ const Blog = (props) => {
             :
             <div>
               {blog.map((blogitem) => {
-                return <div key={blogitem.title}>
+                return (
+                <div key={blogitem.title}>
                   <Link href={URL+`/blogpost/${blogitem.slug}`} passHref>
                     <h2>{blogitem.title} </h2></Link>
                   <p>{blogitem.content.substr(0, 150)}</p>
                 </div>
-              })}
+                )})}
             </div>
         }
       </main>
